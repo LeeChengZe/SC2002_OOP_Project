6ee0c6ecbec3257bd772de485572d6bfa573ed44
LeeChengZe <72968326+LeeChengZe@users.noreply.github.com>
2026-03-28 22:48:40 +0800

    Update GameCLI.java

--- GameCLI.java
+++ GameCLI.java
@@ -1,4 +1,11 @@
-public class GameCLI {
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
 
-    
+public class GameCLI {
+    private final Scanner scanner;
+
+    public GameCLI() {
+        this.scanner = new Scanner(System.in);
+    }
 }
