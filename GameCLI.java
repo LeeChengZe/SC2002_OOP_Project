0eb33a891e7d6e74795358f22fdfcebc94c29c77
LeeChengZe <72968326+LeeChengZe@users.noreply.github.com>
2026-03-28 23:10:20 +0800

    Update GameCLI.java

--- GameCLI.java
+++ GameCLI.java
@@ -8,4 +8,29 @@ public class GameCLI {
     public GameCLI() {
         this.scanner = new Scanner(System.in);
     }
+
+    /* 
+       User input checking function 
+       Function takes in a min and max value and ensures that the value stays within range
+       Function ensures that only integer data type are accepted 
+    */
+    private int readIntInRange(int min, int max) {
+        while (true) {
+            try {
+                System.out.print("Input: ");
+                int inputValue = scanner.nextInt();
+                scanner.nextLine();
+
+                if (inputValue < min || inputValue > max) {
+                    System.out.println("Invalid input range. Please enter a number between " + min + " and " + max + ".");
+                    continue;
+                }
+                return inputValue;
+
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input type. Please enter a number.");
+                scanner.nextLine();
+            }
+        }   
+    }
 }
