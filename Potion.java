e77ec330c7a09c0378ca8588b622957c69958c1b
LeeChengZe <72968326+LeeChengZe@users.noreply.github.com>
2026-03-30 23:53:50 +0800

    Create Potion.java

---
+++ Potion.java
@@ -0,0 +1,17 @@
+public class Potion implements Item {
+    private final String name = "Potion";
+    private final int healAmount = 100;
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void use(Combatant user, Combatant target, BattleEngine engine) {
+        user.heal(healAmount);
+        engine.getBattleLog().addMessage(
+            user.getName() + " uses Potion and heals " + healAmount + " HP!"
+        );
+    }
+}
