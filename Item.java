4c0969bd936c19d172af835f4c64a7c062d259dd
LeeChengZe <72968326+LeeChengZe@users.noreply.github.com>
2026-03-30 23:42:28 +0800

    Create Item.java

---
+++ Item.java
@@ -0,0 +1,7 @@
+/*
+*  Blueprint that forces every item in the game to have a name and a use method
+*/
+public interface Item {
+    String getName();
+    void use(Combatant user, Combatant target, BattleEngine engine);
+}
