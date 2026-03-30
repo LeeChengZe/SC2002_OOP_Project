f8bf946f99ba26987f43ab57850636da8ee43edb
LeeChengZe <72968326+LeeChengZe@users.noreply.github.com>
2026-03-30 23:32:13 +0800

    Create BattleLog.java

---
+++ BattleLog.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BattleLog {
+    private final List<String> messages;
+
+    /*
+    * Constructor - Create an Array List to store the message
+    */
+    public BattleLog() {
+        this.messages = new ArrayList<>();
+    }
+    /*
+    * Function to add the message into the list
+    */
+    public void addMessage(String message) {
+        messages.add(message);
+        System.out.println(message);
+    }
+
+    /*
+    * Function to return the list of stored message
+    */
+    public List<String> getMessages() {
+        return messages;
+    }
+}
