4bcc71bc59b38726d1f436a91bb8e2e86743dc62
pranathichijs-ai <pranathichijs@gmail.com>
2026-04-01 21:35:56 +0800

    Update EnemyActionStrategy.java

--- EnemyActionStrategy.java
+++ EnemyActionStrategy.java
@@ -1,3 +1,4 @@
+// to choose which action to perform for an enemy, allowing for different strategies to be implemented for different types of enemies.
 public interface EnemyActionStrategy{
     Action decide(Enemy enemy);
 }
