ecffb41a2c2c377b89be91d687d78400d3a7dc43
pranathichijs-ai <pranathichijs@gmail.com>
2026-04-01 21:42:49 +0800

    comments added
    comments added

--- UseItemAction.java
+++ UseItemAction.java
@@ -1,3 +1,4 @@
+// This class implements the Action interface and defines the behavior for using an item in combat. When executed, it checks if the actor is an instance of Player and, if so, it uses the first item in the player's inventory on the actor. This allows the player to utilize items during combat, such as healing potions or buffs, to gain an advantage over their opponent.
 public class UseItemAction implements Action {
     public void execute(Combatant actor, Combatant target) {
         if (actor instanceof Player) {
