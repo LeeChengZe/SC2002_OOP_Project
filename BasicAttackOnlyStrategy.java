9d1d93bb088e062ccd95ae88b2388d710e9675ad
LeeChengZe <72968326+LeeChengZe@users.noreply.github.com>
2026-04-03 20:38:57 +0800

    Update BasicAttackOnlyStrategy.java

--- BasicAttackOnlyStrategy.java
+++ BasicAttackOnlyStrategy.java
@@ -1,6 +1,7 @@
-// This class implements the EnemyActionStrategy interface and defines a strategy where the enemy will always choose to perform a basic attack action. The chooseAction method returns a new instance of BasicAttackAction, which can be executed by the enemy during combat.
+// This class implements the EnemyActionStrategy interface and defines a strategy where the enemy will always choose to perform a basic attack action.
 public class BasicAttackOnlyStrategy implements EnemyActionStrategy {
-    public Action chooseAction(Enemy enemy) {
-        return new BasicAttackAction();
+    @Override
+    public Action chooseAction(Enemy enemy, BattleEngine engine, Player player) {
+        return new BasicAttackAction(player);
     }
 }
